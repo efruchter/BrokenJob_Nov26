620f4afa2a0682cccf1927050088177041b35a9c
efruchter <eric@funktroniclabs.com>
2018-11-26 11:43:55 -0800

    first commit

---
+++ Assets/BrokenJob.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Burst;
+
+namespace Fuji.JellyPhysics
+{
+    using Unity.Collections;
+    using Unity.Jobs;
+    using Unity.Mathematics;
+    
+    [BurstCompile]
+    public struct BrokenJob : IJob
+    {
+        public NativeArray<float3> GrabConstraints;
+
+        [ReadOnly] public float3x3 LeftHandPosition, RightHandPosition;
+
+        public void Execute()
+        {
+            for (int grabIndex = 0; grabIndex < GrabConstraints.Length; grabIndex++)
+            {
+                bool isLeft = grabIndex == 0 || grabIndex == 2;
+                var pos = isLeft ? LeftHandPosition.c0 : RightHandPosition.c0;
+                int grabStatKey = isLeft ? 1 : 2;
+            }
+        }
+    }
+}
